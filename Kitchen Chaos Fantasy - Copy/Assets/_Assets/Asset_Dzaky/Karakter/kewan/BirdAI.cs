b709722ce4a1432dcce401c8d61eb35367f019d0
Rashelyo123 <rashelyosyawalian31@gmail.com>
2025-06-07 21:43:05 +0700

    manok

--- Kitchen Chaos Fantasy - Copy/Assets/_Assets/Asset_Dzaky/Karakter/kewan/BirdAI.cs
+++ Kitchen Chaos Fantasy - Copy/Assets/_Assets/Asset_Dzaky/Karakter/kewan/BirdAI.cs
@@ -1,18 +1,63 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BirdAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 3f;
+    public float waitTimeMin = 2f;
+    public float waitTimeMax = 4f;
+
+    public float flyRadius = 10f; // Radius area terbang
+    public float minFlyHeight = 5f;
+    public float maxFlyHeight = 10f;
+
+    public Transform centerPoint; // Titik pusat area terbang (bisa Empty GameObject di tengah pulau)
+
+    private Vector3 targetPosition;
+    private Animator animator;
+    private bool isFlying = true;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        animator.SetBool("isFlying", true);
+
+        PickNewTarget();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        FlyToTarget();
+    }
+
+    void FlyToTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetPosition - transform.position), 2f * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetPosition) < 0.5f)
+        {
+            StartCoroutine(WaitAndPickNewTarget());
+        }
+    }
+
+    IEnumerator WaitAndPickNewTarget()
+    {
+        isFlying = false;
+        float waitTime = Random.Range(waitTimeMin, waitTimeMax);
+        yield return new WaitForSeconds(waitTime);
+        PickNewTarget();
+        isFlying = true;
+    }
+
+    void PickNewTarget()
+    {
+        Vector3 randomOffset = new Vector3(
+            Random.Range(-flyRadius, flyRadius),
+            Random.Range(minFlyHeight, maxFlyHeight),
+            Random.Range(-flyRadius, flyRadius)
+        );
+
+        targetPosition = centerPoint.position + randomOffset;
     }
 }
