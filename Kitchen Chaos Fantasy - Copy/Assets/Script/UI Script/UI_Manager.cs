3b2f99fe6aeb7f88ebd2bd820981598d77283060
Rashelyo123 <rashelyosyawalian31@gmail.com>
2025-06-08 20:00:38 +0700

    update

--- Kitchen Chaos Fantasy - Copy/Assets/Script/UI Script/UI_Manager.cs
+++ Kitchen Chaos Fantasy - Copy/Assets/Script/UI Script/UI_Manager.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour
 {
     public GameObject PauseMenu;
-
+    public Button ResumeButton;
 
     private void Update()
     {
@@ -23,20 +24,34 @@ public class UI_Manager : MonoBehaviour
             }
         }
     }
+
     public void PauseGame()
     {
         PauseMenu.SetActive(true);
-        Time.timeScale = 0f; // Pause the game
+        Time.timeScale = 0f;
         Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None; // Unlock the cursor
+        Cursor.lockState = CursorLockMode.None;
+
+
+        EventSystem.current.SetSelectedGameObject(null);
+
+        if (ResumeButton != null)
+        {
+            Animator anim = ResumeButton.GetComponent<Animator>();
+            if (anim != null)
+            {
+                anim.Update(0f);
+                anim.Play("Normal", 0);
+            }
+        }
     }
 
     public void ResumeGame()
     {
         PauseMenu.SetActive(false);
-        Time.timeScale = 1f; // Resume the game
+        Time.timeScale = 1f;
         Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked; // Lock the cursor
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void QuitGame()
